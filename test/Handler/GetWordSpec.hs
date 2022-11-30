6c30c27e97a52b88592aee5c18a76aec871b7faa
s388chen <chenshu1996@yahoo.com>
2022-11-30 15:07:28 -0500

    modified and restructured tests

--- test/Handler/GetWordSpec.hs
+++ test/Handler/GetWordSpec.hs
@@ -4,15 +4,16 @@
 module Handler.GetWordSpec (spec) where
 
 import Foundation
-import Handler.GetWord
 import TestImport
 
 spec :: Spec
 spec = withApp $ do
   describe "getGetWordR" $ do
-    it "gives a 200" $ do
+    it "gives a 200 and display the correct form" $ do
       get GetWordR
       statusIs 200
+      htmlAnyContain "h1" "Our word recommendation"
+      htmlAnyContain "label" "Your term here. You can make a guess."
 
   describe "invalid requests" $ do
     it "400s when Post request - Invalid Method" $ do
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,1 @@
 {-# OPTIONS_GHC -F -pgmF hspec-discover #-}
-
-import Lib
-
-main :: IO ()
-main = do
-  putStrLn someFunc
-  putStrLn "Test suite not yet implemented"
