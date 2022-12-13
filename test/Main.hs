213ce27e98867833387e54356e2127a9d2852705
pang-xingxing <njmskyx@outlook.com>
2022-12-13 16:20:57 -0500

    load dotenv in test

--- test/Main.hs
+++ test/Main.hs
@@ -1,13 +1,15 @@
 module Main where
 
 import AutocorrectTest
+import Configuration.Dotenv (defaultConfig, loadFile)
 import Spec
-import Test.Hspec.Runner
+import Test.Hspec.Runner hiding (defaultConfig)
 
 main :: IO ()
 main = do
+  putStrLn "*** Testing Specs ***"
+  _ <- loadFile defaultConfig
+  hspec Spec.spec
   putStrLn "*** Testing Autocorrect ***"
   _ <- test_all
   qc
-  putStrLn "*** Testing Specs ***"
-  hspec Spec.spec
